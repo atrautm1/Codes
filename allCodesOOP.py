4d84b6a4cfc03d46db17bbf9412bdc54585a5d73
atrautm1 <atrautm1@uncc.edu>
2017-05-26 10:30:26 -0400

    added anagram code and changed

--- allCodesOOP.py
+++ allCodesOOP.py
@@ -1,4 +1,23 @@
+#!/usr/local/bin/python3
+
 """These are the combination of all the codes that I have written,
 rewritten in Object Oriented Programming style... It's a work in progress"""
 
+#Test if two strings are anagrams..
+class anagram():
 
+    def __init__(self,str1,str2):
+        self.str1 = str1
+        self.str2 = str2
+        self.test_anagram(self.str1,self.str2)
+
+    def test_anagram(self,str1,str2):
+        str1List = list(str1.upper())
+        str2List = list(str2.upper())
+        if len(str1) != len(str2):
+            print("They are not equal in length!")
+        else:
+            if str1List.sort() is str2List.sort():
+                print("The two strings are anagrams")
+            else:
+                print("No, the two strings are not anagrams")
--- anagram.py
+++ anagram.py
@@ -1,19 +1,15 @@
+#!/usr/local/bin/python3
+
 def anagram(str1,str2):
     str1List = list(str1.upper())
     str2List = list(str2.upper())
     print (str1List, str2List)
     if len(str1) != len(str2):
-        print("They not equal")
+        print("They are not equal in length!")
     else:
-#        for x in str1.upper():
-#            str1List.append(x)
-#            print (str1List)
-#        for z in str2.upper():
-#            str2List.append(z)
-#            print (str2List)
         if str1List.sort() is str2List.sort():
             print("The two strings are anagrams")
         else:
-            print("Nein!")
+            print("No, the two strings are not anagrams")
 
 anagram(input("What's your first string? "),input("What's your second string? "))
